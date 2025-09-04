616fab4e250e369dcd011ba078b8b0d478329e71
Praneeth Reddy Karrem <praneethreddykarrem123@gmail.com>
2025-09-04 17:45:29 +0530

    added

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
-<<<<<<< HEAD
   production: true,
-  backendUrl: 'https://story-app-backend-37gk.onrender.com', // Update this with your actual backend URL
-  aiServiceUrl: 'http://ai-service-app-env.eba-bmapzmun.ap-south-1.elasticbeanstalk.com', // Update this with your actual AI service URL
-=======
-  production: false,
-  backendUrl: 'https://story-app-backend-37gk.onrender.com',
-  aiServiceUrl: 'http://ai-service-app-env.eba-bmapzmun.ap-south-1.elasticbeanstalk.com',
->>>>>>> d9eb17ac6617b9059c7e19a3126c7f378f2ffce1
+  backendUrl: 'https://story-app-backend-37gk.onrender.com', // This is the correct URL
+  aiServiceUrl: 'http://story-app-ai-env.eba-bmapzmun.ap-south-1.elasticbeanstalk.com', // Use your correct AI service URL here
   apiVersion: 'v1'
 };
-
