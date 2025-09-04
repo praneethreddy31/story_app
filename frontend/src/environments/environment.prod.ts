d9eb17ac6617b9059c7e19a3126c7f378f2ffce1
Praneeth Reddy Karrem <149233969+praneethreddy31@users.noreply.github.com>
2025-09-04 17:21:58 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  backendUrl: 'https://story-app-backend-37gk.onrender.com/',
+  backendUrl: 'https://story-app-backend-37gk.onrender.com',
   aiServiceUrl: 'http://ai-service-app-env.eba-bmapzmun.ap-south-1.elasticbeanstalk.com',
   apiVersion: 'v1'
 };
+
