1b347d7f7f9c232cafe0783a4214ce872e70c963
Praneeth Reddy Karrem <praneethreddykarrem123@gmail.com>
2025-09-04 20:56:20 +0530

    changes in prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://story-app-backend-37gk.onrender.com', // This is the correct URL
+  backendUrl: 'https://story-app-backend-37gk.onrender.com', 
   aiServiceUrl: 'http://story-app-ai-env.eba-bmapzmun.ap-south-1.elasticbeanstalk.com', // Use your correct AI service URL here
   apiVersion: 'v1'
 };
